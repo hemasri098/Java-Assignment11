9e869c1f4afa36d7a2d151ad0e68a2cd5f88f851
hemasri098 <59275471+hemasri098@users.noreply.github.com>
2020-03-26 11:09:05 +0530

    added exception handling and loggers

--- Assignment11.java
+++ Assignment11.java
@@ -1,43 +1,53 @@
-package com.company;
+package com.company.JavaAssignment11;
 import java.io.*;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 public class Assignment11 {
+    public static Logger LOGGER = Logger.getAnonymousLogger();
+    //storing character counts in a file
     public static HashMap<Character, Integer> characterCount(BufferedReader reader) throws IOException {
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> charactercount = new HashMap<Character, Integer>();
         String input = "";
         while((input = reader.readLine()) != null) {
+            //making count of each character
             for (int i = 0; i < input.length(); i++) {
-                if (!map.containsKey(input.charAt(i))) {
-                    map.put(input.charAt(i), 1);
+                if (!charactercount.containsKey(input.charAt(i))) {
+                    charactercount.put(input.charAt(i), 1);
                 } else {
-                    map.put(input.charAt(i), map.get(input.charAt(i)) + 1);
+                    charactercount.put(input.charAt(i), charactercount.get(input.charAt(i)) + 1);
                 }
             }
         }
-        return map;
+        return charactercount;
     }
     public static void main(String[] args) throws IOException {
-        if(args.length > 0) {
-            File file = new File(args[0]);
-            FileInputStream fileStream = new FileInputStream(file);
-            InputStreamReader input = new InputStreamReader(fileStream);
-            BufferedReader reader = new BufferedReader(input);
-           // System.out.println(characterCount(reader));
-            HashMap<Character, Integer> myMap = characterCount(reader);
-            try{
-                FileWriter fw = new FileWriter("/home/dhemasri/Desktop/assignment11.txt");
-                String result = "";
-                for(Character key : myMap.keySet()) {
-                    result += key + " - " + myMap.get(key) + "\n";
+        //reading command line argument
+        try {
+            if (args.length > 0) {
+                File file = new File(args[0]);
+                FileInputStream fileStream = new FileInputStream(file);
+                InputStreamReader input = new InputStreamReader(fileStream);
+                BufferedReader reader = new BufferedReader(input);
+                HashMap<Character, Integer> charactercount = characterCount(reader);
+                try {
+                    //text file to store output
+                    FileWriter fw = new FileWriter("/home/dhemasri/Desktop/assignment11.txt");
+                    String result = "";
+                    for (Character key : charactercount.keySet()) {
+                        result += key + " - " + charactercount.get(key) + "\n";
+                    }
+                    //writing output into text file
+                    fw.write(result);
+                    fw.close();
+                } catch (Exception e) {
+                    LOGGER.info("Exception: " + e);
                 }
-                fw.write(result);
-                fw.close();
+                LOGGER.info("Result is saved into the text file assignment11.txt");
             }
-            catch(Exception e) {
-                System.out.println(e);
-            }
-            System.out.println("Result is saved into the text file assignment11.txt");
+        }
+        catch (Exception e) {
+            LOGGER.info("Exception: " + e);
         }
     }
     }
